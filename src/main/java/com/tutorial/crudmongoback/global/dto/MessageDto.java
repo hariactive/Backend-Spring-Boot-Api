5196fbac6f82dfd2761376ed68faa8fb06896458
HARI NARAYAN SHUKLA <hari7706shukla@gmail.com>
2023-04-12 01:36:03 +0530

    first commit

---
+++ src/main/java/com/tutorial/crudmongoback/global/dto/MessageDto.java
@@ -0,0 +1,34 @@
+package com.tutorial.crudmongoback.global.dto;
+
+import org.springframework.http.HttpStatus;
+
+public class MessageDto {
+
+    private HttpStatus status;
+    private String message;
+
+    public MessageDto() {
+
+    }
+
+    public MessageDto(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
